629760f5ac9ec80c92fd347e635025b18cd527b8
mjrchiphazard <kidbest100@gmail.com>
2024-10-26 13:29:57 -0700

    Update custom_text_header.js

--- css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
+++ css/addons/unraid/login-page/retro-terminal/js/custom_text_header.js
@@ -6,11 +6,13 @@
 // YOU MUST ESCAPE ANY SINGLE BACKSLASHES LIKE SO: \\
 
 let custom_text_header = `
-<div class="custom-text-header"> <pre>                     __                             ___       __  
-   ____  ____  _____/ /__________  ____ ___  ____  / (_)___  / /__
-  / __ \\/ __ \\/ ___/ __/ ___/ __ \\/ __ '__ \\/ __ \\/ / / __ \\/ //_/
- / / / / /_/ (__  ) /_/ /  / /_/ / / / / / / /_/ / / / / / / ,&lt;   
-/_/ /_/\\____/____/\\__/_/   \\____/_/ /_/ /_/\\____/_/_/_/ /_/_/|_|  
+<div class="custom-text-header"> <pre>      .o.            ooooo      ooo      ooooooooooooo        .oooooo.        ooooo      ooo 
+     .888.           `888b.     `8'      8'   888   `8       d8P'  `Y8b       `888b.     `8' 
+    .8"888.           8 `88b.    8            888           888      888       8 `88b.    8  
+   .8' `888.          8   `88b.  8            888           888      888       8   `88b.  8  
+  .88ooo8888.         8     `88b.8            888           888      888       8     `88b.8  
+ .8'     `888.        8       `888            888           `88b    d88'       8       `888  
+o88o     o8888o      o8o        `8           o888o           `Y8bood8P'       o8o        `8
                                                                  </pre> </div>
 `;
 document.getElementById("login").innerHTML += custom_text_header
